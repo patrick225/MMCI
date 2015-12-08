cc0ab540b6f13335f4a6491c39ec2c1c006bd36f
Nico Wagner <nicolas.wagner7@gmx.net>
2015-12-08 10:59:22 +0100

    polarcoordinates added

--- app/src/main/java/com/maya/androidtutorial/smartwatchtest/Coordinates.java
+++ app/src/main/java/com/maya/androidtutorial/smartwatchtest/Coordinates.java
@@ -1,7 +1,29 @@
 package com.maya.androidtutorial.smartwatchtest;
 
+import static java.lang.Math.atan2;
+import static java.lang.Math.sqrt;
+
 /**
  * Created by patrick on 08.12.15.
  */
 public class Coordinates {
+
+    int screenSize;
+    int xnew;
+    int ynew;
+
+
+    public Coordinates(int screenSize){
+        this.screenSize = screenSize;
+    }
+    public PolarCoordinates getPositionPolar(int xabs, int yabs){
+
+        int r = (int) sqrt(xabs^2+yabs^2);
+        xnew = xabs - r;
+        ynew = yabs + r;
+        double phi = atan2(ynew, xnew);
+
+        return new PolarCoordinates(r,phi);
+
+    }
 }
---
+++ app/src/main/java/com/maya/androidtutorial/smartwatchtest/PolarCoordinates.java
@@ -0,0 +1,15 @@
+package com.maya.androidtutorial.smartwatchtest;
+
+/**
+ * Created by nicolas on 08.12.2015.
+ */
+public class PolarCoordinates {
+
+    int radius;
+    double angle;
+
+    public PolarCoordinates(int radius, double angle){
+        this.radius = radius;
+        this.angle = angle;
+    }
+}
